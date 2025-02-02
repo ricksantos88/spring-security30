9187ad470346a4f6c4c359534e8c163c22c76e47
Wendel Santos <wendel.santos@consultores.rnp.br>
2025-02-01 23:15:16 -0300

    first commit

---
+++ src/main/kotlin/br/com/wendel/springsecurity30/Springsecurity30Application.kt
@@ -0,0 +1,11 @@
+package br.com.wendel.springsecurity30
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springsecurity30Application
+
+fun main(args: Array<String>) {
+	runApplication<Springsecurity30Application>(*args)
+}
